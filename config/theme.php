556e8deb408234e96d4a884c771c04017661a2bd
hoangdv <hoangdv@qsoft.com.vn>
2017-02-23 16:26:08 +0700

    Ignore config/theme.php

--- config/theme.php
+++ config/theme.php
@@ -1,13 +1,8 @@
 <?php
 $config = [];
 
-<<<<<<< HEAD
 $config['active'] ='smallpine';
 $config['active_id'] ='1';
-=======
-$config['active'] ='popo';
-$config['active_id'] ='2';
->>>>>>> development
 $config['driver'] ='file';
 
 return $config;
