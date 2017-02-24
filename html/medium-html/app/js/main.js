65fb8a41ef922b753f6d1588cee9ab9027bc1313
Nghiem Tinh Thuong <nghiemtinhthuong@gmail.com>
2017-02-24 16:10:03 +0700

    Up medium-html home page

---
+++ html/medium-html/app/js/main.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function($) {
+
+    $('.header-slide').owlCarousel({
+        items: 1,
+        nav: true,
+        navText: ["<img src='images/icon/icon-prev.png'>","<img src='images/icon/icon-next.png'>"],
+    });
+
+    $('.tips-slide').owlCarousel({
+        items: 3,
+        margin: 30,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 100, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+
+});
